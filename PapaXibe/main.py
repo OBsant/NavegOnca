6bbeb01646faa542b28f086531ecdf6d5dc954a2
Bruno <ocivaldobruno.nascimento@gmail.com>
2022-05-01 21:41:23 -0300

    Abertura do banco

--- PapaXibe/main.py
+++ PapaXibe/main.py
@@ -1,15 +1,36 @@
+from colorama import Cursor
 from flask import Flask, render_template, request
+import pymysql
 
 app = Flask(__name__)
+
+# db = pymysql.connect(host="db4free.net", user="admin_uniz", password="senha123", database="proj_integrado")
+# cursor = db.cursor()
+
+# cursor.execute("DROP TABLE IF EXISTS User")
+
+# sql = '''CREATE TABLE User(
+#     nome CHAR(50) NOT NULL,
+#     contato CHAR(50) NOT NULL,
+#     senha CHAR(50) NOT NULL,
+#     preco CHAR(50) NOT NULL,
+#     descricao CHAR(50) NOT NULL
+# )'''
+
+# cursor.execute(sql)
+
+# db.close()
 
 """ Configurações para pagina de login/delete da aplicação """
 # Apagar algo do banco de dados
 @app.route("/index", methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        return render_template("list.html")
-    else:
+    if request.method == 'GET':
         return render_template("index.html")
+    else:
+        contato = request.form.get("contato")
+        print(contato)
+        return render_template("list.html")
 
 
 """Configurações para pagina de list da aplicação"""
@@ -24,6 +45,32 @@ def list():
 @app.route("/cadastrar", methods=['GET', 'POST'])
 def cadastrar():
     if request.method == 'POST':
+        app = Flask(__name__)
+
+        nome = request.form.get("nome")
+        senha = request.form.get("senha")
+        preçoparaotransporte = request.form.get("preçoparaotransporte")
+        descricao = request.form.get("descricao")
+        emailounumerodecelular = request.form.get("emailounumerodecelular")
+
+        print(nome, senha, preçoparaotransporte, descricao, emailounumerodecelular)
+
+        db = pymysql.connect(host="db4free.net", user="admin_uniz", password="senha123", database="proj_integrado")
+        cursor = db.cursor()
+
+        sql = f"""INSERT INTO User(nome, contato, senha, descricao, preco)
+                    VALUES ('{nome}', '{emailounumerodecelular}', '{senha}', '{descricao}', '{preçoparaotransporte}')"""
+
+        try:
+            cursor.execute(sql)
+            db.commit()
+        except:
+            print("erro caralho")
+            db.rollback()
+            # TA DANDO ERRO NESSA PORRA SLA PQ
+
+        db.close()
+
         return render_template("list.html")
     else:
         return render_template("cadastrar.html")
